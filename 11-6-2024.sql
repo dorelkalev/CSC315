0b6b74a8321ec9f99e2f1c4514d1a049de49a48b
dorelkalev <158530164+dorelkalev@users.noreply.github.com>
2024-11-06 19:32:35 -0500

    Create 11-6-2024.sql

---
+++ 11-6-2024.sql
@@ -0,0 +1,21 @@
+-- Storage + Indexing 
+
+-- Students
+-- ID: 
+-- Name:
+-- Major:
+
+CREATE TABLE Students (
+  ID INT,
+  Name VARCHAR(50),
+  Major CHAR(10)
+);
+
+-- Students[10];
+
+-- r = Students[9];
+
+-- Columnar
+-- ID[10]
+-- Name[10]
+-- Major[10]
